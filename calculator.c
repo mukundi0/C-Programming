f9424baf72936ba5c0cb7f30d510b3168ade0d13
Mukundi <165059960+mukundi0@users.noreply.github.com>
2024-05-12 15:59:31 +0300

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main(){
+
+    char operator;
+    double num1;
+    double num2;
+    double result;
+
+    printf("Enter an operator (+ - * - /): \n");
+    scanf("%c",&operator);
+
+    printf("Enter Number 1: ");
+    scanf("%lf",&num1);
+
+    printf("Enter Number 2: ");
+    scanf("%lf",&num2);
+
+    switch (operator)
+    {
+    case '+':
+        result = num1 + num2;
+        printf("\nResult: %.2lf ",result);
+        break;
+    case '-':
+        result = num1 - num2;
+        printf("\nResult: %.2lf ",result);  
+        break;  
+    case '*':
+        result = num1 * num2;
+        printf("\nResult: %.2lf ",result);
+        break;
+    case '/':
+        result = num1 / num2;
+        printf("\nResult: %.2lf ",result);
+        break;
+    default:
+        printf("%c is not valid!");
+        break;
+    }
+
+
+    return 0;
+}
