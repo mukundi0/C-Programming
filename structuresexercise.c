f46940255fe24108ceb7fa10f82f02879fa6c545
Mukundi <165059960+mukundi0@users.noreply.github.com>
2024-07-02 11:21:30 +0300

    Add files via upload

---
+++ structuresexercise.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct CDAccount
+{
+    double balance;
+    double interest_rate;
+    int terms;
+}CDAccount;
+
+//funcion to display and compute
+void get_data(struct CDAccount the_account){
+
+    printf("Account terms (number of months): %d\n", the_account.terms);
+    
+    double interest = the_account.balance * (the_account.interest_rate / 100 ) * (the_account.terms / 12.0);
+
+    double computed_balance = the_account.balance + interest;
+
+    printf("The lnterest rate: %.2f\n", the_account.interest_rate);
+
+    printf("The lnitial balance: %.2f\n", the_account.balance);
+
+    printf("The balance after %d months: %.2f", the_account.terms, computed_balance);
+}
+
+int main(){
+    
+    //values entered
+    printf("Enter the balance: ");
+    scanf("%lf",&CDAccount.balance);
+    printf("Enter the interest rate: ");
+    scanf("%lf",&CDAccount.interest_rate);
+    printf("Enter the terms (number of months): ");
+    scanf("%d",&CDAccount.terms);
+
+    //call the function
+    get_data(CDAccount);
+
+    return 0;
+
+}
