15ff01053540f5f3afc5c89c811bae4d5cc63a1b
Mukundi <165059960+mukundi0@users.noreply.github.com>
2024-07-02 12:00:37 +0300

    structuresexercise.c

--- structuresexercise.c
+++ structuresexercise.c
@@ -8,40 +8,18 @@ struct CDAccount
     int terms;
 }CDAccount;
 
-//funcion to display and compute
-void get_data(struct CDAccount the_account){
+//funcion to display and compute, function prototype
+void get_data(struct CDAccount the_account);
 
-    printf("Account terms (number of months): %d\n", the_account.terms);
-    
-    double interest = the_account.balance * (the_account.interest_rate / 100 ) * (the_account.terms / 12.0);
+int main(){
 
-    double computed_balance = the_account.balance + interest;
-
-    printf("The lnterest rate: %.2f\n", the_account.interest_rate);
-
-    printf("The lnitial balance: %.2f\n", the_account.balance);
-
-    printf("The balance after %d months: %.2f", the_account.terms, computed_balance);
-}
-
-void input_data(struct CDAccount){
+    //values entered
     printf("Enter the balance: ");
     scanf("%lf",&CDAccount.balance);
     printf("Enter the interest rate: ");
     scanf("%lf",&CDAccount.interest_rate);
     printf("Enter the terms (number of months): ");
     scanf("%d",&CDAccount.terms);
-}
-
-int main(){
-    
-    //values entered
-    /*printf("Enter the balance: ");
-    scanf("%lf",&CDAccount.balance);
-    printf("Enter the interest rate: ");
-    scanf("%lf",&CDAccount.interest_rate);
-    printf("Enter the terms (number of months): ");
-    scanf("%d",&CDAccount.terms);*/
     
     //Alternative call a function to enter values
     input_data(CDAccount);
@@ -51,4 +29,19 @@ int main(){
 
     return 0;
 
+}
+void get_data(struct CDAccount the_account){
+    {
+
+    printf("Account terms (number of months): %d\n", the_account.terms);
+    
+    double interest = the_account.balance * (the_account.interest_rate / 100 ) * (the_account.terms / 12.0);
+
+    double computed_balance = the_account.balance + interest;
+
+    printf("The lnterest rate: %.2f\n", the_account.interest_rate);
+
+    printf("The lnitial balance: $%.2f\n", the_account.balance);
+
+    printf("The balance after %d months: $%.2f", the_account.terms, computed_balance);
 }
